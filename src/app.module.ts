38621e651d231183da2f51e4ae6f92f7611603f1
Yongjun Park <yopark@student.42seoul.kr>
2024-03-10 18:59:18 +0900

    init: nest g res users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { ResumesModule } from './resumes/resumes.module';
 import { ClientsModule } from './clients/clients.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TodosModule, ResumesModule, ClientsModule],
+  imports: [TodosModule, ResumesModule, ClientsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
