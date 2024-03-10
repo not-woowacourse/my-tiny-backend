e8409fcab63b29af186e6715d3702bb34862eefd
Yongjun Park <yopark@student.42seoul.kr>
2024-03-11 00:42:36 +0900

    feat(not-woowacourse-admin-guard): draft

---
+++ src/auth/not-woowacourse-admin.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class NotWoowacourseAdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    const password =
+      request.headers['Not-Woowacourse-Admin-Password'.toLowerCase()];
+
+    // TODO: password must be stored in .env
+    return password === 'kucc-2024';
+  }
+}
--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { NotWoowacourseAdminGuard } from 'src/auth/not-woowacourse-admin.guard';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 
+@UseGuards(NotWoowacourseAdminGuard)
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
