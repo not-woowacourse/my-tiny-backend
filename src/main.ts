a0aa0ef9cc225594d67c1159ec17ed4e2a93e549
Yongjun Park <yopark@student.42seoul.kr>
2024-03-11 03:43:24 +0900

    feat(cors): enable cors on localhost 3000 ~ 3002 port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+    ],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
