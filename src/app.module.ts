8a7ba5eefe313c5561bbc47cf0494a26f42ff75e
Yongjun Park <yopark@student.42seoul.kr>
2024-03-10 20:01:32 +0900

    chore(db): add settings (test)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from 'src/todos/entities/todo.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
-import { ResumesModule } from './resumes/resumes.module';
 import { ClientsModule } from './clients/clients.module';
+import { ResumesModule } from './resumes/resumes.module';
+import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TodosModule, ResumesModule, ClientsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Todo],
+      }),
+    }),
+    TodosModule,
+    ResumesModule,
+    ClientsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,1 +1,20 @@
-export class Todo {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
