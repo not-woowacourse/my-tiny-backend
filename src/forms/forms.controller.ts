c64d3da68a9211fbc2e832ef1fe1cc41fb370581
Yongjun Park <yopark@student.42seoul.kr>
2024-05-10 04:24:51 +0900

    init: nest g res forms

---
+++ src/forms/forms.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+
+import { CreateFormDto } from './dto/create-form.dto';
+import { UpdateFormDto } from './dto/update-form.dto';
+import { FormsService } from './forms.service';
+
+@Controller('forms')
+export class FormsController {
+  constructor(private readonly formsService: FormsService) {}
+
+  @Post()
+  create(@Body() createFormDto: CreateFormDto) {
+    return this.formsService.create(createFormDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.formsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.formsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFormDto: UpdateFormDto) {
+    return this.formsService.update(+id, updateFormDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.formsService.remove(+id);
+  }
+}
