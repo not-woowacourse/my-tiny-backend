f98c7f66299fc371e2694162c5e760ec32c3680c
Yongjun Park <yopark@student.42seoul.kr>
2024-06-03 20:43:41 +0900

    chore(cors): add link (searzh-시연용)

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,25 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      /* localhost */
       'http://localhost:10242',
+
+      /* 시연용 */
       'https://not-woowacourse-1-toodoo-frontend-for-example.vercel.app',
       'https://not-woowacourse-2-surveey-frontend-for-example.vercel.app',
+      'https://not-woowacourse-3-searzh-frontend-for-example.vercel.app',
+
+      /* 도구 */
       'https://not-woowacourse-2-surveey-admin.yopark.dev',
+
+      /* 과제 제출자 */
       'https://not-woowacourse.te6.in',
       'https://not-woowacourse-toodoo.yopark.dev',
       'https://not-woowacourse-1-toodoo-frontend-jet.vercel.app',
       'https://nextjs-session-1-toodoo.vercel.app',
       'https://one-hope.vercel.app',
       'https://2-surveey-frontend-umber.vercel.app',
+      'https://not-woowacourse-surveey.yopark.dev',
     ],
   });
 
